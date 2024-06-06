be4814953b07311b4be4590075aefcff4f744357
SandeepMarumudi <marumudisandeep@gmail.com>
2024-06-06 13:30:15 +0530

    conditional staements problems

---
+++ script.js
@@ -0,0 +1,195 @@
+// first task  teas
+// console.log("welcome to restaurant")
+// console.log("menu")
+// console.log("1.black tea-10")
+// console.log("2.green tea-20")
+// console.log("3.coffee-30")
+// select=+prompt("select your menu by giving 1,2,3")
+// if(select==1)
+//     {
+//         console.log("you selected black tea")
+//     }
+//     else if(select==2)
+//     {
+//         console.log("you selected green tea")
+//     }
+//     else{
+//         console.log("you selected cofee")
+//     }
+// quantity=+prompt("no.of teas")
+// if(select==1)
+// {
+// console.log("your bill is="+10*quantity)
+// console.log("your bill after discount"+ (1-0.05)*10*quantity)
+// }
+// else if(select==2)
+// {
+// console.log("your bill is="+20*quantity)
+// console.log("your bill after discount"+ (1-0.05)*20*quantity)
+// }
+// else{
+// console.log("your bill is="+30*quantity)
+// console.log("your bill after discount"+ (1-0.05)*30*quantity)
+// }
+
+//second task employes salry
+
+// console.log("Employees role and salary")
+// salary=+prompt("enter salary")
+// if(salary>=60000)
+//     {
+//         console.log("Employee is Developer")
+//     } 
+//     else if(salary>=40000 && salary<60000)
+//         {
+//             console.log("Employee is a designer")
+//         }
+//         else{
+//             console.log("Employee is a tester")
+//         }
+
+
+//third task minor and major
+
+
+// age=+prompt("Enter age of a person ")
+// if(age>=60){
+//     console.log("Senior citizen")
+// }
+// else if(age>=18 && age<60)
+//     {
+//         console.log("Adult")
+//     }
+//     else{
+//         console.log("minor")
+//     }
+
+//fourth task discount
+
+
+// purchase=+prompt("Enter purchase amount")
+// if(purchase>=1000)
+//     {
+//         console.log("Discount:10%")
+//     }
+//     else if(purchase>=500 && purchase<1000)
+//         {
+//             console.log("discount:10%")
+//         }
+//         else{
+//             console.log("discount:5%")
+//         }
+
+
+
+// fivth task weekend week day
+
+// day=prompt("Enter day")
+// if(day=="saturday" || day=="sunday")
+//     {
+//         console.log("Weekend")
+//     }
+//     else if(day=="monday")
+//         {
+//             console.log("weekday")
+//         }
+//         else if(day=="Tuesday")
+//             {
+//                 console.log("weekday")
+//             }
+
+//             else if(day=="wednesday")
+//                 {
+//                     console.log("weekday")
+//                 }
+
+//                 else if(day=="Thursday")
+//                     {
+//                         console.log("weekday")
+//                     }
+
+//                     else if(day=="friday")
+//                         {
+//                             console.log("weekday")
+//                         }
+
+//                         else
+//                             {
+//                                 console.log("Invalid")
+//                             }
+
+                            
+
+
+//sixth task odd or even
+
+
+
+// number=+prompt("enter number")
+// if(number%2==0)
+//     {
+//     console.log("even")
+//     }
+//     else
+//     {
+//         console.log("odd")
+//     }
+
+
+
+
+// seventh task leap year
+
+// year=Number(prompt("enter the year "))
+// if(year%100==0 && year%400==0)
+//     {
+//     console.log("it is leap year")
+//     }
+//     else if(year%100!==0 && year%4==0)
+//         {
+//             console.log("it is leap year")
+//         }
+//         else
+//         {
+//             console.log("it is not leap year")
+//         }
+
+
+
+//eighth task ovwels and consonents
+
+// char=prompt("enter alphabet")
+// if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u")
+//     {
+//         console.log("it is ovwel")
+//     }
+// else{
+//     console.log("it is consonant")
+// }
+
+
+
+//ninth task divisible by 3 and 5
+
+
+// number=Number(prompt("enter number"))
+// if(number%3==0 && number%5==0)
+//     {
+//         console.log("it is divisible by 3 and 5")
+//     }
+// else if(number%3==0 && number%5!==0)
+//         {
+//             console.log("it is divisible by 3")
+//         }
+// else if(number%5==0 && number%3!==0)
+//     {
+//         console.log("it is divisible by 5")
+//     }
+// else if(number%3!==0 && number%5!==0)
+//     {
+//         console.log("it is not divisible by 3 or 5")
+//     }
+
+
+
+
